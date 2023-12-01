63dcbe22688dddb2ddc33fb9f511f7b867b1fed5
Sangram8897 <59616997+Sangram8897@users.noreply.github.com>
2023-12-01 23:23:45 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,104 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+    <Icon name="rocket" size={30} color="#900" />
+    <Text style={styles.sectionTitle}>
+    rocket
+</Text>
+<Text style={styles.sectionTitle2}>rocket</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    fontFamily:'DMSans-Bold',
+    color:'red'
+  },
+  sectionTitle2: {
+    fontSize: 24,
+    fontWeight: '600',
+    // fontFamily:'DMSans-Bold',
+    // color:'red'
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ app/root/index.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AppNavigationStack from './navigation'
+
+const App = () => {
+  return (
+    <AppNavigationStack/>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
